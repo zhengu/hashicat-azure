3f1356d9c0b769b66b5349c90ee7df8066ccc4e6
Zhen Gu <zhengu@gmail.com>
2022-02-09 20:06:32 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/ZhenGu/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "azurerm_resource_group.example.name"
+  resource_group_name = "zhengu-workshop"
 }
